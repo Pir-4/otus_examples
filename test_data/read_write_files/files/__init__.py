30a34d4c27c6f706b78dd8dae448e56d464aec49
Valentin Agapitov <valentin.agapitov@exness.com>
2024-01-19 09:34:43 +0200

    fix

--- test_data/read_write_files/files/__init__.py
+++ test_data/read_write_files/files/__init__.py
@@ -1,18 +1,18 @@
 import os
 
 
-def get_pah(file_name):
+def get_path(file_name):
     work_folder = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(work_folder, file_name)
 
 
-TXT_FILE = get_pah('poem.txt')
-CSV_FILE = get_pah('users.csv')
-JSON_FILE = get_pah('users.json')
-YAML_FILE = get_pah('config.yaml')
+TXT_FILE = get_path('poem.txt')
+CSV_FILE = get_path('users.csv')
+JSON_FILE = get_path('users.json')
+YAML_FILE = get_path('config.yaml')
 
-TXT_FILE_W = get_pah('poem_w.txt')
-CSV_FILE_W = get_pah('users_w.csv')
-JSON_FILE_W = get_pah('users_w.json')
-YAML_FILE_W = get_pah('config_w.yaml')
+TXT_FILE_W = get_path('poem_w.txt')
+CSV_FILE_W = get_path('users_w.csv')
+JSON_FILE_W = get_path('users_w.json')
+YAML_FILE_W = get_path('config_w.yaml')
 
