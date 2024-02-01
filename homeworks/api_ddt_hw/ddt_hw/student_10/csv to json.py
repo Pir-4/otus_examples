86f1076e9d3e81c7bd889579b985241ceacb57d8
Valentin Agapitov <valentin.agapitov@exness.com>
2024-02-01 13:33:32 +0200

    student 10

--- homeworks/api_ddt_hw/ddt_hw/student_10/csv to json.py
+++ homeworks/api_ddt_hw/ddt_hw/student_10/csv to json.py
@@ -1,7 +1,7 @@
 import json
 import csv
-
-from files_hw import JSON_FILE, JSON_FILE_W, CSV_FILE
+from csv import DictReader
+from files_hw import JSON_FILE, CSV_FILE
 
 # открываю json, записываю из него данные
 result = []
@@ -9,20 +9,18 @@ with open(JSON_FILE, "r") as f:
     users = json.load(f)
     for i in users:
         result.append({"name": i['name'], "gender": i['gender'], "address": i['address'],
-                       "age": i['age'], "Book": []})
-        len(result)
+                       "age": i['age'], "book": []})
+#        len(result)
 
 # преобразую csv не с помощью DictReader чтобы сразу убрать
-# Издательство и не писать потом больше строк прописывая заголовки
 with open(CSV_FILE, newline='') as f2:
-    reader = csv.reader(f2)
+    reader = DictReader(f2)
     array_dict = []
-    # Извлекаю заголовок
-    header = next(reader)[:4]
+
     # Итерируюсь по данным, делая из них словари
     for row in reader:
-        csv_dict = dict(zip(header, row))  # делает словарь с ключом
-        array_dict.append(csv_dict)  # словарь книг
+        array_dict.append(row)  # словарь книг
+        # print("array_dict", array_dict)
 
 user_count = len(result)  # кол-во пользователей
 
@@ -30,9 +28,16 @@ user_count = len(result)  # кол-во пользователей
 counter = 0
 for book in array_dict:
     current_user_index = counter % user_count  # вычисляю индекс текущего пользователя
-    result[current_user_index]['Book'].append(book)  # добавляю книги пользователям
+    result[current_user_index]['book'].append(  # добавляю книги пользователям
+        {
+            'title': book['Title'],
+            'author': book['Author'],
+            'pages': book['Pages'],
+            'genre': book['Genre']
+        }
+    )
     counter += 1
 
-with open(JSON_FILE_W, "w") as f:
-    s = json.dumps(result, indent=4)  # записываю
+with open('result.json', "w") as f:
+    s = json.dumps(result, indent=4)  # записываю в файл результаты
     f.write(s)
